d07932a408c402536b6f601d53da9a991a16ca21
rodpepablo <rodpepablo@gmail.com>
2022-07-19 16:48:30 +0200

    ADD: tests for router

--- test/router.test.js
+++ test/router.test.js
@@ -1,2 +1,25 @@
 
-test.todo('asdf')
+import { ToDoController } from '../src/controllers/todo.js'
+import { Router } from '../src/router.js'
+
+class TestableRouter extends Router {
+    constructor (mock) {
+        super()
+        this.toDoController = mock
+    }
+}
+
+test('should build the controllers', () => {
+    const router = new Router()
+    expect(router.toDoController).toBeInstanceOf(ToDoController)
+})
+
+test('should connect controllers to the router', () => {
+    const toDoController = { use: jest.fn() }
+    const router = new TestableRouter(toDoController)
+    const app = jest.fn()
+    
+    router.connect(app)
+
+    expect(toDoController.use).toHaveBeenCalledWith(app)
+})
